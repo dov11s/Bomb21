d40e5b310ba72def59b34eea98f4aa3c9a0043e1
Paulius Ratkevičius <pauliuz95@gmail.com>
2021-10-07 20:10:41 +0300

    Wall

--- src/main/java/shared/GameObject.java
+++ src/main/java/shared/GameObject.java
@@ -1,5 +1,32 @@
 package shared;
 
-public class GameObject {
-    public int id;
+public abstract class GameObject {
+    public boolean isWalkable;
+    public boolean isDestroyable;
+
+    public String color;
+    public float alpha;
+
+    public GameObject(String color, float alpha) {
+        super();
+        this.alpha = alpha;
+        this.color = color;
+    }
+
+    public void sayHello() {
+      System.out.println("GameObject");
+    }
+
+    public void setDestroyable(boolean option){
+        this.isDestroyable = option;
+    }
+
+    public void setWalkable(boolean option){
+        this.isWalkable = option;
+    }
+
+    public abstract void onDamage();
+    public abstract void onTick();
+    public abstract void onStep();
+
 }
---
+++ src/main/java/shared/Wall.java
@@ -0,0 +1,19 @@
+package shared;
+
+public class Wall extends GameObject{
+
+    public Wall(String color, float alpha){
+        super(color, alpha);
+
+    }
+
+    public void onDamage(){
+
+    }
+    public void onTick(){
+
+    }
+    public void onStep(){
+
+    }
+}
