7319371691017f173d5c414b50a0b8c9f2c0041e
Antanas Stepanauskas <antanas.stepanauskas@ktu.edu>
2021-10-07 20:53:13 +0300

    pushing gameboard

--- src/main/java/shared/GameBoard.java
+++ src/main/java/shared/GameBoard.java
@@ -4,14 +4,24 @@ import java.util.ArrayList;
 
 public class GameBoard {
 
-    public final int sizeX = 20;
-    public final int sizeY = 20;
+    public final int size = 1000;
+    public final int gridSize = 20;
     public GameObject[][] objects;
 
 
     public GameBoard()
     {
-        this.objects = new GameObject[sizeX][sizeY];
+        this.objects = new GameObject[gridSize][gridSize];
         this.objects[5][5] = new Wall("AAAAAA",1);
+    }
+    public void runTick()
+    {
+        for (GameObject[] u: objects) 
+        {
+            for (GameObject object: u)
+            {
+            	object.onTick();
+            }
+        }
     }
 }
